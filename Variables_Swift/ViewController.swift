8e8ad8b1e4e68a8127ee9db1d4173b05da51e600
OfentseLearnsCode <malebyeoc@gmail.com>
2023-06-28 07:57:11 +0200

    Initial Commit

---
+++ Variables_Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Variables_Swift
+//
+//  Created by DA MAC M1 137 on 2023/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
